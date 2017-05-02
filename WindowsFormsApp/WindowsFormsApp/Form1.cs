913ec88517bf65eb773e79128e798671dbbf2f53
Prokofiev Alexander <Alexandr.Prokofiev.A@gmail.com>
2017-05-03 02:57:54 +0300

    add table output

--- WindowsFormsApp/WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp.Model;
 
 namespace WindowsFormsApp
 {
@@ -15,6 +17,30 @@ namespace WindowsFormsApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'logModelDataSet.Logs' table. You can move, or remove it, as needed.
+            this.logsTableAdapter.Fill(this.logModelDataSet.Logs);
+            LogModel db = new LogModel();
+            var logs = db.Logs.Include(l => l.File).Include(l => l.HTTPMethod).Include(l => l.IPAdress1)
+                .Select(l => new DataBindingProjection()
+                {
+                    IPAdress = l.IPAdress,
+                    IPAdressCompanyName = l.IPAdress1.NAme,
+                    FileTitle = l.File.PageName,
+                    PathAndName = l.File.PathAndName,
+                    HTTPMethod = l.HTTPMethod.Name,
+                    HTTPStatus = l.HTTPStatus
+                });
+            dataGridView1.DataSource = logs.ToList();
+            dataGridView1.Columns[0].DataPropertyName = "IPAdress";
+            dataGridView1.Columns[1].DataPropertyName = "IPAdressCompanyName";
+            dataGridView1.Columns[2].DataPropertyName = "FileTitle";
+            dataGridView1.Columns[3].DataPropertyName = "PathAndName";
+            dataGridView1.Columns[4].DataPropertyName = "HTTPMethod";
+            dataGridView1.Columns[5].DataPropertyName = "HTTPStatus";
         }
     }
 }
---
+++ WindowsFormsApp/WindowsFormsApp/Model/DataBindingProjection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp.Model
+{
+    class DataBindingProjection
+    {
+        public string IPAdress { get; set; }
+
+        public string IPAdressCompanyName { get; set; }
+
+        public string FileTitle { get; set; }
+
+        public string PathAndName { get; set; }
+
+        public string HTTPMethod { get; set; }
+
+        public int? HTTPStatus { get; set; }
+    }
+}
